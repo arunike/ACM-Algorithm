a89df958bfe1dff1aa92f20185197e4f768d617a
Ziye Zhang <zzhang2293@outlook.com>
2024-01-09 20:22:42 -0600

    bug

--- frontend/src/pages/login-credentials/index.js
+++ frontend/src/pages/login-credentials/index.js
@@ -1,15 +1,22 @@
 import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+const id = '';
 
 function LoginCredentials() {
+  const router = useRouter();
+
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
-    console.log(urlParams);
+    // console.log(urlParams);
 
     const token = urlParams.get('token');
+    id = urlParams.get('id');
+    localStorage.setItem('token', token);
     console.log(token);
-
-
   }, []);
+
+  router.push(`http://localhost:3000?id=${id}`);
 
   return (
     <div>
